0075ca1e11f4c812af72a550fe805347df67f690
Karolis Šarapnickis <karolis.sarapnickis@nordsec.com>
2021-01-06 12:37:40 +0200

    fix: title

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js + Preact!</a>
         </h1>
 
         <p>
